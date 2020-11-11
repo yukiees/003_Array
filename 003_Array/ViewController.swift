b7a2731406fce2e0e9d6976e7fba8c64fabbde42
yukiees <yukiii8989@gmail.com>
2020-11-11 15:05:44 +0900

    Initial Commit

---
+++ 003_Array/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  003_Array
+//
+//  Created by 松島優希 on 2020/11/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ 003_ArrayTests/_03_ArrayTests.swift
@@ -0,0 +1,33 @@
+//
+//  _03_ArrayTests.swift
+//  003_ArrayTests
+//
+//  Created by 松島優希 on 2020/11/11.
+//
+
+import XCTest
+@testable import _03_Array
+
+class _03_ArrayTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
