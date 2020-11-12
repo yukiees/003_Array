50a1ee09aa01981f0483fdbe5b45efdc942ec339
yukiees <yukiii8989@gmail.com>
2020-11-13 01:45:53 +0900

    UIset

--- 003_Array/ViewController.swift
+++ 003_Array/ViewController.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var itsuLabel: UILabel!
+    @IBOutlet weak var daregaLabel: UILabel!
+    @IBOutlet weak var dokodeLabel: UILabel!
+    @IBOutlet weak var doshitaLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func change(_ sender: Any) {
+        
+    }
+    
+    @IBAction func reset(_ sender: Any) {
+        
+    }
+    
+    @IBAction func random(_ sender: Any) {
+        
+    }
+    
+    
+    
+    
 
 }
-
